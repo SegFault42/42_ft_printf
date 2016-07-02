fb2a5112c8f84ed3b0075b7b3d511099c3075895
SegFault42 <rabougue@student.42.fr>
2016-07-02 18:26:52 +0200

    %.0% en cours

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -81,11 +81,6 @@ int	percent_percent(const char *format, int ret)
 	num[i] = 'a';
 	nb_char = ft_atoi(num);
 	len_format = ft_strclen(format, '%') + 1;
-	if (format[0] == '-')
-	{
-		ft_putchar('%');
-		ret++;
-	}
 	while (len_format < nb_char--)
 	{
 		ft_putchar(' ');
@@ -96,6 +91,34 @@ int	percent_percent(const char *format, int ret)
 		ft_putchar('%');
 		ret++;
 	}
+	return (ret);
+}
+
+int	percent_percent_neg(const char *format, int ret)
+{
+	char num[10];
+	int i = 0;
+	int nb_char = 0;
+	int	len_format;
+
+	while (ft_isdigit(*format) == 1)
+	{
+		num[i] = *format;
+		format++;
+		i++;
+	}
+	num[i] = 'a';
+	nb_char = ft_atoi(num);
+	len_format = ft_strclen(format, '%') + 1;
+	ft_putchar('%');
+	format++;
+	ret++;
+	while (len_format < nb_char--)
+	{
+		ft_putchar(' ');
+		ret++;
+	}
+
 	return (ret);
 }
 
@@ -118,9 +141,19 @@ int	ft_printf(const char *format, ...)
 				format++;
 			if (*format == '%' || *format == '-' || ft_isdigit(*format) == 1)
 			{
-				ret += percent_percent(format, ret);
-				while (ft_isdigit(*format) == 1)
+				if (*format == '-')
+				{
 					format++;
+					ret += percent_percent_neg(format, ret);
+					while (ft_isdigit(*format) == 1)
+						format++;
+				}
+				else
+				{
+					ret += percent_percent(format, ret);
+					while (ft_isdigit(*format) == 1)
+						format++;
+				}
 			}
 			/*else if (*++format == 's')*/
 				/*ret += percent_s(pa, ret);*/
