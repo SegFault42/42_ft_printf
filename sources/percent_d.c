3682ea55b86f968071b0bded407385772f22bf3b
SegFault42 <rabougue@student.42.fr>
2016-08-05 08:05:53 +0200

    13 fail

--- sources/percent_d.c
+++ sources/percent_d.c
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/25 00:56:11 by rabougue          #+#    #+#             */
-/*   Updated: 2016/08/05 07:49:35 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/08/05 08:02:51 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,12 +91,14 @@ const char	*percent_d(va_list pa, t_printf *print, const char *format)
 	neg_sign = 0;
 	if (print->space_number > 0)
 		neg_sign = 1;
-	if (print->plus == 1 && print->precision_zero > 0)
+	if (print->plus == 1 && print->precision_zero > 0 && d > 0)
 	{
 		ft_putchar('+');
 		--print->precision_zero;
 		++print->ret;
 	}
+	if (d < 0 && print->plus == 1)
+		print->precision_zero--;
 	if (print->space_number > 0 && print->precision_zero > 0)
 	{
 		print->space_number -= print->precision_zero;
